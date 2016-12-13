08fe2daf3c2e9320d39dbad44a5107a446767870
Richard Littauer <richard.littauer@gmail.com>
2016-12-13 11:13:53 -0500

    Automatically get flags

--- cli.js
+++ cli.js
@@ -21,6 +21,18 @@ const cli = meow([`
   }
 })
 
+var dir = process.cwd().split('/')
+
 if (process.argv[2]) {
+  // TODO Figure out how to get the directory from the argument
+  // Only use when in a directory with the README
+  if (cli.input[0].split('/').length === 1) {
+    if (!cli.flags.project) {
+      cli.flags.project = dir[dir.length - 2]
+    }
+    if (!cli.flags.repo) {
+      cli.flags.repo = dir[dir.length - 1]
+    }
+  }
   checkFileContents(process.argv[2], cli.flags)
 }
