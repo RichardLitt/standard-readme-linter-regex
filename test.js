f522a787c122903195ebdd25e9d64facc6434502
Richard Littauer <richard.littauer@gmail.com>
2016-12-18 12:37:12 -0500

    Fixed tests, removed old option, remove dead test

--- test.js
+++ test.js
@@ -8,39 +8,33 @@ const input = `# standard-linter
 // TODO Add Unit tests
 
 describe('validate file', () => {
-  it('reads some file', function() {
-    const data = validate(input, './standard-linter/README.md', 'none')
-    assert(data !== null, "File should exist.")
+  it('reads some file', function () {
+    const data = validate(input, './standard-linter/README.md')
+    assert(data !== null, 'File should exist.')
   })
 
   it('wants it to be called README', function() {
-    const data = validate(input, './standard-linter/README.md', 'none')
+    const data = validate(input, './standard-linter/README.md')
     assert.equal(data.README, true)
   })
 
   it('wants it to not be called REAMDE', function() {
-    const data = validate(input, './standard-linter/REAMDE.md', 'none')
+    const data = validate(input, './standard-linter/REAMDE.md')
     assert.equal(data.README, false)
   })
 
   it('needs a markdown format', function() {
-    const data = validate(input, './standard-linter/REAMDE.md', 'none')
+    const data = validate(input, './standard-linter/REAMDE.md')
     assert.equal(data.markdown, true)
   })
 
   it('only accepts .md', function() {
-    const data = validate(input, './standard-linter/REAMDE.markdown', 'none')
+    const data = validate(input, './standard-linter/REAMDE.markdown')
     assert.equal(data.markdown, false)
   })
 
   it('accepts no other file types', function() {
-    const data = validate(input, './standard-linter/REAMDE.txt', 'none')
+    const data = validate(input, './standard-linter/REAMDE.txt')
     assert.equal(data.markdown, false)
-  })
-
-  it('checks the code of conduct link', () => {
-    input = '[Code of Conduct](https://github.com/ipfs/community/blob/master/code-of-conduct.md)'
-    const data = validate(input, 'README.md')
-    assert.equal(data.cocLink, true)
   })
 })
